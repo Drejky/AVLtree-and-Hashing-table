4d7416d599898000f93063814e51e31af1a246f4
Drejky <peter.kudela9@gmail.com>
2020-04-06 19:56:34 +0100

    Fixing the logic

--- main.c
+++ main.c
@@ -4,12 +4,12 @@
 #include <string.h>
 #include <time.h>
 #define COUNT 10
+int flag = 0;
 
 typedef struct foo {
 	int val;
 	int height;
 
-	foo* parent;
 	foo* left;
 	foo* right;
 }Node;
@@ -46,34 +46,38 @@ int max(int a, int b) {
 		return b;
 }
 
-//Gets the length of the longest branch in tree
-int height(Node* curr, int bal) {
+int height(Node* curr) {
 	if (!curr)
-		return bal;
-	return max(height(curr->left, bal+1), height(curr->right, bal+1));
-
-}
-
-int getBalance(Node* curr) {
-	return height(curr->left, 0) - height(curr->right, 0);
-}
-
-void fixParent(Node* curr) {
-	if (!curr->parent)
-		return;
-	if (curr->parent->val < curr->val)
-		curr->parent->right = curr;
+		return 0;
 	else
-		curr->parent->left = curr;
+		return curr->height;
+}
+
+
+int treeBalance(Node* curr) {
+	int temp;
+	if (!curr)
+		return 0;
+	else {
+		temp = height(curr->left) - height(curr->right);
+		if (temp > 1 || temp < -1)
+			flag = 1;
+		curr->height = temp;
+	}
+	if (curr->right)
+		treeBalance(curr->right);
+	if (curr->left)
+		treeBalance(curr->left);
+
 }
 
 //Create a new node
-Node* newNode(Node* parent, int val) {
+Node* newNode(int val) {
 	Node* temp = (Node*)malloc(sizeof(Node));
 	temp->right = NULL;
 	temp->left = NULL;
 	temp->val = val;
-	temp->parent = parent;
+	temp->height = 0;
 	return temp;
 }
 
@@ -84,9 +88,10 @@ Node* LLrotate(Node *curr) {
 
 	newRoot->right = curr;
 	newRoot->right->left = rightChild;
-	newRoot->parent = curr->parent;
-	curr->parent = newRoot;
-	fixParent(newRoot);
+
+	curr->height = 1 + max(height(curr->left), height(curr->right));
+	newRoot->height = 1 + max(height(newRoot->left), height(newRoot->right));
+	
 	return newRoot;
 	
 }
@@ -96,9 +101,11 @@ Node* RRrotate(Node *curr) {
 
 	newRoot->left = curr;
 	newRoot->left->right = leftChild;
-	newRoot->parent = curr->parent;
-	curr->parent = newRoot;
-	fixParent(newRoot);
+
+	curr->height = 1 + max(height(curr->left), height(curr->right));
+	newRoot->height = 1 + max(height(newRoot->left), height(newRoot->right));
+	
+
 	return newRoot;
 
 }
@@ -107,13 +114,14 @@ Node* LRrotate(Node *curr) {
 	Node* leftChild = newRoot->left;
 	Node* rightChild = newRoot->right;
 	newRoot->left = curr->left;
-	newRoot->left->parent = newRoot;
 	newRoot->right = curr;
 	newRoot->left->right = leftChild;
 	newRoot->right->left = rightChild;
-	newRoot->parent = curr->parent;
-	curr->parent = newRoot;
-	fixParent(newRoot);
+
+	curr->height = 1 + max(height(curr->left), height(curr->right));
+	newRoot->left->height = 1 + max(height(newRoot->left->left), height(newRoot->left->right));
+	newRoot->height = 1 + max(height(newRoot->left), height(newRoot->right));
+
 	return newRoot;
 }
 Node* RLrotate(Node *curr) {
@@ -121,38 +129,41 @@ Node* RLrotate(Node *curr) {
 	Node* leftChild = newRoot->left;
 	Node* rightChild = newRoot->right;
 	newRoot->right = curr->right;
-	curr->right->parent = newRoot;
 	newRoot->left = curr;
 	newRoot->left->right = leftChild;
 	newRoot->right->left = rightChild;
-	newRoot->parent = curr->parent;
-	curr->parent = newRoot;
-	fixParent(newRoot);
+
+	curr->height = 1 + max(height(curr->left), height(curr->right));
+	newRoot->right->height = 1 + max(height(newRoot->right->left), height(newRoot->right->right));
+	newRoot->height = 1 + max(height(newRoot->left), height(newRoot->right));
+
 	return newRoot;
 }
 
+
 Node* balance(Node* curr, int val) {
 	int bal;
-	while (curr->parent) {
-		curr = curr->parent;
-		bal = getBalance(curr);
-		if (bal < -1) {//R
-			if (val > curr->right->val) {//RR
-				return curr = RRrotate(curr);
-			}
-			else {
-				return curr = RLrotate(curr);
-			}
+	bal = height(curr->left) - height(curr->right);
+	if (bal < -1) {//R
+		if (val > curr->right->val) {//RR
+			return RRrotate(curr);
 		}
-		else if (bal > 1) {//L
-			if (val < curr->left->val) {//LL
-				return curr = LLrotate(curr);
-			}
-			else {
-				return curr = LRrotate(curr);
-			}
+		else if (val < curr->right->val) {
+			return RLrotate(curr);
 		}
 	}
+	else if (bal > 1) {//L
+		if (val < curr->left->val) {//LL
+			return LLrotate(curr);
+		}
+		else if (val > curr->left->val) {
+			return LRrotate(curr);
+		}
+	}
+	else
+		return curr;
+
+	
 }
 
 //Insert function
@@ -160,36 +171,25 @@ Node* insert(Node *curr, int val) {
 	//Simple binary tree insertion
 	if (curr) {
 		if (curr->val > val) {
-			if (curr->left == NULL) {
-				curr->left = newNode(curr, val);
-				curr = balance(curr, val);
-			}
-			else {
-				curr = insert(curr->left, val);
-			}
+			curr->left = insert(curr->left, val);
 		}
 		else if (curr->val < val) {
-			if (curr->right == NULL) {
-				curr->right = newNode(curr, val);
-				curr = balance(curr, val);
-			}
-			else {
-				curr = insert(curr->right, val);
-			}
+			curr->right = insert(curr->right, val);
 		}
 	}
 	else {
-		curr = newNode(NULL, val);
+		return newNode(val);
 	}
 	
-	while (curr->parent)
-		curr = curr->parent;
+	if (curr->val > val) {
+		curr->height = max(height(curr->left) + 1, height(curr));
+	}
+	else if (curr->val < val) {
+		curr->height = max(height(curr), height(curr->right) + 1);
+	}
 
-	
-	/*
-	printf("\n____________%d\n",val);
-	print2DUtil(curr, 0);
-	*/
+	curr = balance(curr, val);
+
 	return curr;
 }
 
@@ -215,13 +215,24 @@ int rando(int low, int high) {	//random numbers for testing
 	return rand() % (high - low + 1) + low;
 }
 
+void del_tree(Node* curr) {
+	if (!curr)
+		return;
+	if (curr->left)
+		del_tree(curr->left);
+	if (curr->right)
+		del_tree(curr->right);
+	free(curr);
+}
+
 int main() {
 	Node* start = NULL;
 
 	int temp;
 	
-	for (int i = 0; i < 1000; i++) {
-		temp = rando(0, 1000);
+	srand(time(NULL));
+	for (int i = 0; i < 1000000; i++) {
+		temp = rando(0, 2000000);
 		if (!(search(start, temp)))
 			start = insert(start, temp);
 	}
@@ -242,11 +253,11 @@ int main() {
 	*/
 	//RRrotate(start);
 	
-	
-	printf("\n____________%d\n", start->parent);
-	print2DUtil(start, 0);
-	
+	treeBalance(start);/*
+	printf("\n____________\n");
+	print2DUtil(start, 0);*/
+	printf("%d", flag);
 
-	
+	del_tree(start);
 	return 0;
 }
